3f53af82ccf4d457136af69db1dbb88a316cd0d6
zeeshanpaalo <zeeshanpaalo@gmail.com>
2025-01-25 17:14:54 +0545

    type for context

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { InlineKeyboard } from "grammy";
+import { Context, InlineKeyboard } from "grammy";
 import { Actions } from "./constants";
 
 const { Bot } = require("grammy");
@@ -7,7 +7,7 @@ const { Bot } = require("grammy");
 const bot = new Bot(process.env.BOT_TOKEN);
 
 // Show options when the bot starts
-bot.command("start", async (ctx: any) => {
+bot.command("start", async (ctx: Context) => {
   const keyboard = new InlineKeyboard()
     .text("Check Solana Balance", Actions.CHECK_SOL_BALANCE)
     .text("Parse Raydium Transaction", Actions.PARSE_TX);
@@ -18,19 +18,19 @@ bot.command("start", async (ctx: any) => {
 });
 
 // Register listeners to handle messages
-bot.on("message:text", (ctx: any) => {
+bot.on("message:text", (ctx: Context) => {
   console.log("User Input");
-  ctx.reply("Echo: " + ctx.message.text);
+  ctx.reply("Echo: " + ctx.message?.text);
 });
 
 // Handle button clicks (callback queries)
-bot.on("callback_query:data", async (ctx: any) => {
-  const action = ctx.callbackQuery.data;
+bot.on("callback_query:data", async (ctx: Context) => {
+  const action = ctx.callbackQuery?.data;
   console.log("callback in play");
   if (action === Actions.CHECK_SOL_BALANCE) {
     // Handle the Check Balance button click
     await ctx.answerCallbackQuery();
-    // TODO: handle getting address for sol balance 
+    // TODO: handle getting address for sol balance
     await ctx.reply("Your balance is: $123.45");
   } else if (action === Actions.PARSE_TX) {
     // Handle the Check Transactions button click
@@ -39,10 +39,9 @@ bot.on("callback_query:data", async (ctx: any) => {
     await ctx.reply("Your last transaction: -$45.67 on 2025-01-20");
   } else {
     // Handle unexpected button actions
-    await ctx.answerCallbackQuery("Unknown action!", { show_alert: true });
+    await ctx.answerCallbackQuery("Unknown action!");
   }
 });
-
 
 bot.catch((err: any) => console.error(err));
 
